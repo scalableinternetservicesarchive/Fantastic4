f827e6732ce1d427d7fb86c8bf4507578107127b
muxin <muxin4ever@gmail.com>
2015-05-02 03:57:55 -0700

    modify fixture and test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
   setup do
-    @event = events(:one)
+    @event = events(:event1)
     @update = {
       title: 'Hello',
       description: 'World',
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   setup do
-    @post = posts(:one)
+    @post = posts(:post1)
   end
 
   test "should get index" do
