1761d3c00618b4092369b3f92a2efb03f86a20bc
Tiffanyyny <tiffanyyny@gmail.com>
2015-05-11 10:11:59 -0700

    fix "unlogged-in user's profile" bug
    unlogged-in user can not access his profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,27 @@
 class UserController < ApplicationController
   def show_organized_events
-    @organized_events = current_user.organized_events
+    if current_user != nil
+      @organized_events = current_user.organized_events
+    else
+      @organized_events = []
+    end
   end
   
 
   def show_voted_events
-    @voted_posts = current_user.voted_posts
+    if current_user != nil
+      @voted_posts = current_user.voted_posts
+    else
+      @voted_posts = []
+    end
   end
 
   def show_created_posts
-    @created_posts = current_user.created_posts
+    if current_user != nil
+      @created_posts = current_user.created_posts
+    else
+      @created_posts = []
+    end
   end
   
   
