d120bc7f5c6134117b6cd31fe4e838d5b3fe40aa
Sunny Zhu <sunnyuny8@gmail.com>
2015-05-15 15:56:58 -0700

    for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,42 +3,49 @@ event1 = Event.create(
 	title: 'School Queen',
 	description: 'Vote for the school queen',
 	tags: 'School',
+  image: File.new("test/fixtures/images/school.jpeg")
 )
 
 event2 = Event.create(
 	title: 'Best Movie',
 	description: 'Vote for the best movie 2014',
 	tags: 'Movie',
+  image: File.new("test/fixtures/images/movie.jpg")
 )
 
 event3 = Event.create(
 	title: 'Pop Star',
 	description: 'best singer ever',
 	tags: 'Music',
+  image: File.new("test/fixtures/images/music.png")
 )
 
 post1 = Post.create(
   event_id: event1.id,
-  text_content: 'Emma',
+  text_content: 'Big Hero 6',
   vote_count: 3,
+  image: File.new("test/fixtures/images/bighero.jpeg")
 )
 
 post2 = Post.create(
-  event_id: event1.id,
-  text_content: 'Julia',
+  event_id: event2.id,
+  text_content: 'Avenger',
   vote_count: 2,
+  image: File.new("test/fixtures/images/avenger.jpg")
 )
 
 post3 = Post.create(
   event_id: event2.id,
   text_content: 'Harry Potter',
   vote_count: 2,
+  image: File.new("test/fixtures/images/1.png")
 )
 
 post4 = Post.create(
   event_id: event3.id,
-  text_content: 'Adele',
+  text_content: 'Transformer',
   vote_count: 1,
+  image: File.new("test/fixtures/images/transformer.jpg")
 )
 
 user1 = User.create(
