d4bdf9276a069a965cccac9fbdfe0e1561ec906a
muxin <muxin4ever@gmail.com>
2015-05-16 05:32:09 -0700

    modify paperclip path url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = {host:'localhost',port:3000}
-  
+
   config.paperclip_defaults = {
-        #:path => ":rails_root/public/images/:id/:filename",
-        :url => "/images/:id/:filename"
+        :path => ":rails_root/public/images/:class/:attachment/:id/:style/:filename",
+        :url => ":class/:attachment/:id/:style/:filename"
       }
 end
