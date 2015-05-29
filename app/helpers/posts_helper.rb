dd1b4edcd2fe9ccef60b894454ce3d5c796b88c7
Sunny Zhu <sunnyuny8@gmail.com>
2015-05-29 16:57:06 -0700

    server caching event and post table and row

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def cache_key_for_post_table
+      "post-table-#{Post.maximum(:updated_at)}-#{Post.maximum(:updated_at)}"
+  end
+  
+  def cache_key_for_post_row(post)
+     "post-#{post.id}-#{post.updated_at}-#{post.vote_count}"
+  end
 end
