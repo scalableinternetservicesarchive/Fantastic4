1f8667596605e047a6c8b65da125040d6cc90bbe
Sunny Zhu <sunnyuny8@gmail.com>
2015-06-04 11:13:55 -0700

    Cache event and post table

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,12 @@
 module EventsHelper
-  # def cache_key_for_event_table
-#       "event-table-#{Event.maximum(:updated_at)}-#{Post.maximum(:updated_at)}"
-#   end
+  def cache_key_for_event_table(events,marker)
+    @date = []
+    @events.each do |event|
+      @date.push(event.updated_at)
+    end
+    puts "HAHA #{@marker}"
+    "event-table-#{@date.max}-#{@marker}"
+  end
   
   
   def cache_key_for_event_row(event)
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,12 @@
 module PostsHelper
-  #why cache this table. When to update the table
-  # def cache_key_for_post_table(posts)
-  #   @date = []
-  #   @posts.each do |post|
-  #     @date.push(post.updated_at)
-  #   end
-  #   "post-table-#{@date.max}"
-  # end
+  def cache_key_for_post_table(posts,event)
+    @date = []
+    id = ""
+    @posts.each do |post|
+      @date.push(post.updated_at)
+    end
+    "post-table-#{@date.max}-#{@event.id}"
+  end
   
   def cache_key_for_post_row(post)
      "post-#{post.id}-#{post.updated_at}-#{post.vote_count}"
