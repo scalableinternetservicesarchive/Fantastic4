16097120447b71233db9c043408722b1b318b4fa
Sunny Zhu <sunnyuny8@gmail.com>
2015-06-04 00:04:23 -0700

    remove server cache from master

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,2 @@
 module EventsHelper
-  def cache_key_for_event_table
-      "event-table-#{Event.maximum(:updated_at)}-#{Event.maximum(:updated_at)}"
-  end
-  
-  
-  def cache_key_for_event_row(event)
-     "event-#{event.id}-#{event.updated_at}-#{event.posts.count}"
-  end
 end
